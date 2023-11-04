9870ee15ea5bb1dfff55f689c34ea1c005ffbdc3
Orfanidis1 <125228441+Orfanidis1@users.noreply.github.com>
2023-11-04 12:22:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function mouseOver1(){
+    document.getElementById('a1').innerHTML = "Computer Setup➡️";
+}
+function mouseLeave1(){
+    document.getElementById('a1').innerHTML = "Computer Setup";
+}
+function mouseOver2(){
+    document.getElementById('a2').innerHTML = "Bathroom➡️";
+}
+function mouseLeave2(){
+    document.getElementById('a2').innerHTML = "Bathroom";
+}
+function mouseOver3(){
+    document.getElementById('a3').innerHTML = "Bedroom➡️";
+}
+function mouseLeave3(){
+    document.getElementById('a3').innerHTML = "Bedroom";
+}
+function mouseOver4(){
+    document.getElementById('a4').innerHTML = "Sisters Bedroom➡️";
+}
+function mouseLeave4(){
+    document.getElementById('a4').innerHTML = "Sisters Bedroom";
+}
+function mouseOver5(){
+    document.getElementById('a5').innerHTML = "Living Room➡️";
+}
+function mouseLeave5(){
+    document.getElementById('a5').innerHTML = "Living Room";
+}
